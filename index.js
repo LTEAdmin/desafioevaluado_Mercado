f61b0f05126a5197f499b867ed982da38b81db2b
Alejandra Flaño <aflanog@latiaencanto.cl>
2024-04-30 07:01:03 -0400

    creacion de rutas estaticas de bootstrap y jquery, jajajaja agregue sugerencias de codeium

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const port = 3000;
 
 //configuracion ruta estatica
 app.use(express.static('assets'));
+app.use("/jquery", express.static("node_modules/jquery/dist"));
+app.use("/bootstrap-js", express.static("node_modules/bootstrap/dist/js"));
+app.use("/bootstrap-css", express.static("node_modules/bootstrap/dist/css"));
+app.use("/popper-js", express.static("node_modules/popper.js/dist"));
+app.use("/fontawesome", express.static("node_modules/@fortawesome/fontawesome-free"));
 
 //Configuracion motor de plantilla
 app.set("view engine", "hbs");
