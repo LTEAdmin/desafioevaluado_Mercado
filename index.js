fbd91dba6a6468f971c78d38a1830ada6cc9975d
Alejandra Flaño <aflanog@latiaencanto.cl>
2024-04-30 06:58:23 -0400

    configuracion ruta estatica assets, configuracion motor de plantilla handlebars

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const express = require('express');
 
 const app = express();
-const port= 3000;
+const port = 3000;
+
+//configuracion ruta estatica
+app.use(express.static('assets'));
+
+//Configuracion motor de plantilla
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname + "/views"));
+app.engine("hbs", exphbs.engine({defaultLayout: "main",})
+);
